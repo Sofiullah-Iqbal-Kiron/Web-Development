0789d2b1d2e6572358d013ffaacba49208992197
Sofiullah Iqbal Kiron <sofiul.k.1023@gmail.com>
2020-10-16 15:32:46 +0600

    Created a new repository for web development

---
+++ JONAS.IO Tutorial/2. JavaScript language basics/ScriptOne.js
@@ -0,0 +1,5 @@
+/*Just write simple JS code in here.*/
+console.log('Hey, Hello There!');
+var firstName = "Sofiullah Iqbal";
+var lastName = prompt("What is your last name?");
+console.log(firstName + ' ' + lastName);
